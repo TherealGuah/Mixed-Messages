4b77fcf71e5f9d50c25aaad73c159b8e6238438c
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2020-12-28 23:24:49 +0000

    Message format created

--- my_message_generator.js
+++ my_message_generator.js
@@ -1,4 +1,22 @@
-let ingredients = ['onion','carrot','tomato',];
 
-let instructions = ['chop the ', 'add the '];
- // chop chop mr bean!
+let wind = {
+    strength: ['Weak','Average','Strong'],
+    direction: ['North','East','South','West'],
+}
+// testing object wind
+// console.log(wind.strength[2]);
+
+
+// message format
+/* 
+1st sentance    
+{greeting}! Today the weather is gonna be {mostly} {clouded} 
+ 2nd
+The wind will blow {north} {weak} //
+3rd
+There is a probability of {30}% {in the afternoon}
+4th
+Hope you enjoin your day! || Better take a umbrella! || Stay home if possible! 
+
+
+*/
