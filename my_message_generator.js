1e6fc8e191437a60701ec39b2b7d0166461b281d
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2020-12-29 16:33:20 +0000

    finished project

--- my_message_generator.js
+++ my_message_generator.js
@@ -58,17 +58,87 @@ const dayRecommendation = () => {
     } else if (skyWeather === 'rainy' && strength === 'strong') {
         recommendation = 'Stay home if possible!';
     } else {
-        recommendation = 'Hope you enjoin your day!';
+        recommendation = 'Hope you enjoy your day!';
     }
     return recommendation;
 };
 // console.log(dayRecommendation()); // returns: one possible string depending on the weather type.
 
 // ASCII art
+const clouded = () => {
+    console.log("                _                                     ");
+    console.log("              (`  ).                   _              ");
+    console.log("             (     ).              .:(`  )`.          ");
+    console.log(")           _(       '`.          :(   .    )         ");
+    console.log("        .=(`(      .   )     .--  `.  (    ) )        ");
+    console.log("       ((    (..__.:'-'   .+(   )   ` _`  ) )         ");
+    console.log("`.     `(       ) )       (   .  )     (   )  ._      ");
+    console.log("  )      ` __.:'   )     (   (   ))     `-'.-(`  )    ");
+    console.log(")  )  ( )       --'       `- __.'         :(      ))  ");
+    console.log(".-'  (_.'          .')                    `(    )  )) ");
+    console.log("                  (_  )                     ` __.:'   ");
+    console.log("                                                      ");
+    console.log("--..,___.--,--'`,---..-.--+--.,,-,,..._.--..-._.-a:f--");
+};
 
+const sunny = () => {
+    console.log("           |");
+    console.log("    \\     |     /");
+    console.log("      \\       /");
+    console.log("         ,d8b,           .,");
+    console.log(" (')-\")_ 88888 ---   ;';'  ';'.");
+    console.log("('-  (. ')98P'      ';.,;    ,;");
+    console.log(" '-.(PjP)'    \\       '.';.'");
+    console.log("           |    \\");
+    console.log("           |");
+};
 
+const rainy = () => {
+    console.log("                                 000      00");
+    console.log("                           0000000   0000");
+    console.log("              0      00  00000000000000000");
+    console.log("            0000 0  000000000000000000000000       0");
+    console.log("         000000000000000000000000000000000000000 000");
+    console.log("        0000000000000000000000000000000000000000000000");
+    console.log("    000000000000000000000000000000000000000000000000");
+    console.log("00000000000000000000000000000000000000000000000000000000");
+    console.log("              / / / / / / / / / / / / / / / /");
+    console.log("            / / / / / / / / / / / / / / /");
+    console.log("            / / / / / / / / / / / / / / /");
+    console.log("          / / / / / / / / / / / / / /");
+    console.log("          / / / / / / / / / / / / /");
+    console.log("        / / / / / / / / / / / /");
+    console.log("        / / / / / / / / / /");
 
+};
 
 
 
 // function to bring everything together.
+const weatherForecast = () => {
+    // 1st
+    console.log(firstPhrase());
+    // 2nd
+    console.log(windCondition());
+    // 3rd
+    console.log(percipitation());
+    // ASCII art
+    switch (skyWeather) {
+        case 'sunny':
+            sunny();
+            break;
+        case 'rainy':
+            rainy();
+            break;
+        case 'clouded':
+            clouded();
+            break;
+        default:
+            console.log(']------------------------------------------------------[');
+            break;
+    };
+    // recommendation
+    console.log(dayRecommendation());
+};
+
+weatherForecast();
