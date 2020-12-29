177628a0b3a95431f4dc44ea474839638c899ee9
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2020-12-29 14:15:27 +0000

    first scentance added

--- my_message_generator.js
+++ my_message_generator.js
@@ -6,8 +6,6 @@ const wind = {
 // testing object wind
 // console.log(wind.strength[2]);
 
-let greeting = ['Hello user!','Good Morning!','Heya!','Hi!!!'];
-
 const rain = {
     timeOfDay: ['this morning','this afternoon','this evening','tonight'],
     percipitation() {
@@ -17,30 +15,31 @@ const rain = {
 // testing method
 // console.log(rain.percipitation());
 
-const weather = {
-    sun: ['clouded','sunny','rainy',],
-    frequancy: ['mostly','fairly','all day','possibly']
-};
  const warningMessage = {
     good: 'Hope you enjoy your day!',
     rain: 'Better take a umbrella!',
     bad: 'Stay home if possible!'
  };
 
+//1st sentance   
+const firstPhrase = () => {
+    const greeting = ['Hello user!','Good Morning!','Heya!','Hi!!!'];
+    const weather = {
+        sky: ['clouded','sunny','rainy',],
+        frequency: ['mostly','fairly','all day','possibly']
+    };
+    let greet = greeting[Math.floor(Math.random() * 4)]; // returns random greeting item
+    // console.log(greet);
+    let skyWeather = weather.sky[Math.floor(Math.random() * 3)];
+    let freqWeather = weather.frequency[Math.floor(Math.random() * 4)];
+    
+    return `${greet} Today the weather is gonna be ${freqWeather} ${skyWeather}.`
+};
+// console.log(firstPhrase()); // returns: "{greeting}! Today the weather is gonna be {frequency} {sky}"
 
 
 
-
-
-
-
-
- 
-// message format
-/* 
-1st sentance    
-{greeting}! Today the weather is gonna be {mostly} {clouded} 
- 2nd
+/*  2nd
 The wind will blow {north} {weak} //
 3rd
 There is a probability of {30}% {in the afternoon}
