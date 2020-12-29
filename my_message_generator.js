3079a186ea34c28bf135adf869b4eb97504733e6
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2020-12-29 01:07:53 +0000

    added objects to compose message

--- my_message_generator.js
+++ my_message_generator.js
@@ -1,12 +1,41 @@
-
-let wind = {
+// object wind
+const wind = {
     strength: ['Weak','Average','Strong'],
     direction: ['North','East','South','West'],
-}
+};
 // testing object wind
 // console.log(wind.strength[2]);
 
+let greeting = ['Hello user!','Good Morning!','Heya!','Hi!!!'];
 
+const rain = {
+    timeOfDay: ['this morning','this afternoon','this evening','tonight'],
+    percipitation() {
+        return Math.floor((Math.random() * 10) * 10);
+    }
+};
+// testing method
+// console.log(rain.percipitation());
+
+const weather = {
+    sun: ['clouded','sunny','rainy',],
+    frequancy: ['mostly','fairly','all day','possibly']
+};
+ const warningMessage = {
+    good: 'Hope you enjoy your day!',
+    rain: 'Better take a umbrella!',
+    bad: 'Stay home if possible!'
+ };
+
+
+
+
+
+
+
+
+
+ 
 // message format
 /* 
 1st sentance    
